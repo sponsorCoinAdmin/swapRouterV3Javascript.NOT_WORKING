5df082d8d450c63799896971ee38fbc73459884b
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-09-07 14:22:45 -0400

    WIP

--- runAlphaRouter.js
+++ runAlphaRouter.js
@@ -4,39 +4,72 @@ const { Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core
 const { ethers, BigNumber } = require('ethers')
 const JSBI  = require('jsbi') // jsbi@3.2.5
 
-const V3_SWAP_ROUTER_ADDRESS = process.env.UNISWAP_SWAPROUTER_02
+let NETWORK
 
-const WALLET_ADDRESS = process.env.WALLET_ADDRESS
-const WALLET_SECRET = process.env.WALLET_SECRET
-const INFURA_TEST_URL = process.env.GOERLI_INFURA_TEST_URL
+let V3_SWAP_ROUTER_ADDRESS = process.env.UNISWAP_SWAPROUTER_02
 
-const web3Provider = new ethers.providers.JsonRpcProvider(INFURA_TEST_URL) // Ropsten
+let WALLET_ADDRESS = process.env.WALLET_ADDRESS
+let WALLET_SECRET = process.env.WALLET_SECRET
+let INFURA_TEST_URL = process.env.GOERLI_INFURA_TEST_URL
 
-const CHAIN_ID = 5
-const router = new AlphaRouter({ chainId: CHAIN_ID, provider: web3Provider})
+let web3Provider = new ethers.providers.JsonRpcProvider(INFURA_TEST_URL) // Ropsten
 
-const name0 = 'Wrapped Ether'
-const symbol0 = 'WETH'
-const decimals0 = 18
-const address0 = process.env.GOERLI_WETH
+// let CHAIN_ID = 5
+let CHAIN_ID = parseInt(process.env.GOERLI_CHAIN_ID)
+
+let router = new AlphaRouter({ chainId: CHAIN_ID, provider: web3Provider})
+
+let name0 = 'Wrapped Ether'
+let symbol0 = 'WETH'
+let decimals0 = 18
+let address0 = process.env.GOERLI_WETH
 // const address0 = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
 
-const name1 = 'Uniswap Token'
-const symbol1 = 'UNI'
-const decimals1 = 18
+let name1 = 'Uniswap Token'
+let symbol1 = 'UNI'
+let decimals1 = 18
 // const address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
 // const address1 = process.env.GOERLI_SPCOIN
-const address1 = process.env.GOERLI_UNI
+let address1 = process.env.GOERLI_UNI
 
-const WETH = new Token(CHAIN_ID, address0, decimals0, symbol0, name0)
-const UNI = new Token(CHAIN_ID, address1, decimals1, symbol1, name1)
+let WETH = new Token(CHAIN_ID, address0, decimals0, symbol0, name0)
+let UNI = new Token(CHAIN_ID, address1, decimals1, symbol1, name1)
 
-const wei = ethers.utils.parseUnits('0.01', 18)
-const inputAmount = CurrencyAmount.fromRawAmount(WETH, JSBI.BigInt(wei))
+let wei = ethers.utils.parseUnits('0.01', 18)
+let inputAmount = CurrencyAmount.fromRawAmount(WETH, JSBI.BigInt(wei))
 
-const bigIntToDecString = ( _value ) => { return bigIntToString(_value, 10); };
-const bigIntToString = ( _value, _base ) => { return BigInt(_value).toString(_base); };
+let bigIntToDecString = ( _value ) => { return bigIntToString(_value, 10); };
+let bigIntToString = ( _value, _base ) => { return BigInt(_value).toString(_base); };
 
+////////////////////////////////////////
+
+async function setNetwork(_network) {
+  NETWORK=_network
+    switch(NETWORK) {
+      case "GOERLI": {
+        CHAIN_ID = parseInt(process.env.GOERLI_CHAIN_ID)
+      }
+      break;
+      case "SEPOLIA":{
+        CHAIN_ID = parseInt(process.env.SEPOLIA_CHAIN_ID)
+      }
+      break;
+      case "MAINNET":{
+        CHAIN_ID = parseInt(process.env.MAINNET_CHAIN_ID)
+      }
+      break;
+      default:{
+        return false
+      }
+    }
+    router = new AlphaRouter({ chainId: CHAIN_ID, provider: web3Provider})
+    WETH = new Token(CHAIN_ID, address0, decimals0, symbol0, name0)
+    UNI = new Token(CHAIN_ID, address1, decimals1, symbol1, name1)
+    let wei = ethers.utils.parseUnits('0.01', 18)
+    let inputAmount = CurrencyAmount.fromRawAmount(WETH, JSBI.BigInt(wei))
+    return true;
+  }
+/////////////////////////////////////////
 async function main() {
   NETWORK=process.env.NETWORK
   console.log("==================================================================================================================================")
